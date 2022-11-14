c6f58995065daa762f1a6e45b4d40e0a5ad64164
fr33zing <fr33zing>
2022-11-14 15:32:43 -0800

    clean util fns

--- newtabgen/src/util.rs
+++ newtabgen/src/util.rs
@@ -4,43 +4,35 @@ use std::{env, fs, io, path::PathBuf};
 
 use sha1::{Digest, Sha1};
 
-/// Finds a suitable cache directory. The directory will be created if needed.
+/// Finds a suitable cache directory.
+fn cache_dir_base() -> Result<PathBuf, io::Error> {
+    let dir = match dirs::cache_dir() {
+        Some(d) => d,
+        None => env::current_dir()?,
+    };
+    Ok(dir.join("newtabgen"))
+}
+
+/// Finds a suitable cache directory. The cache directory will be created if needed.
 ///
 /// # Errors
 ///
 /// Returns an error if a suitable cache directory cannot be found.
 pub fn cache_dir() -> Result<PathBuf, io::Error> {
-    let cache_dir = match dirs::cache_dir() {
-        Some(d) => Ok(d),
-        None => env::current_dir(),
-    }?
-    .join("newtabgen");
-
-    if !cache_dir.exists() {
-        fs::create_dir_all(&cache_dir)?;
-    }
-
+    let cache_dir = cache_dir_base()?;
+    fs::create_dir_all(&cache_dir)?;
     Ok(cache_dir)
 }
 
-/// Finds a suitable cache directory. The directory will be created if needed. Also creates the
-/// request subdirectory if needed.
+/// Finds a suitable cache directory. The cache directory and the requested subdirectory will be
+/// created if needed.
 ///
 /// # Errors
 ///
 /// Returns an error if a suitable cache directory cannot be found.
 pub fn cache_subdir(subdir: &str) -> Result<PathBuf, io::Error> {
-    let cache_dir = match dirs::cache_dir() {
-        Some(d) => Ok(d),
-        None => env::current_dir(),
-    }?
-    .join("newtabgen")
-    .join(subdir);
-
-    if !cache_dir.exists() {
-        fs::create_dir_all(&cache_dir)?;
-    }
-
+    let cache_dir = cache_dir_base()?.join(subdir);
+    fs::create_dir_all(&cache_dir)?;
     Ok(cache_dir)
 }
 
